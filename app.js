44537d1d2b64fefa1644316e3a327949173c6a7c
Sofia <sofia.montgomery@gmail.com>
2022-03-02 17:58:51 +0100

    added highscores, registerscores and auth methods

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const express = require("express");
 const app = express ()
 const PORT = process.env.PORT || 3001
 
+const rug = require('random-username-generator');
+
+let users = [];
+
 app.use('/healthcheck', require('./routes/healthcheck.routes'));
 app.use(express.urlencoded({extended: true}));
 app.use(cors())
@@ -191,6 +195,25 @@ app.get("/levelThree", (req, res)=>{
     res.set("Content-Type", "application/json");
     res.status(200).send(body);
 });
+
+app.get("/registerscore", (req ,res)=>{
+    headers={"http_status":200, "cache-control":  "no-cache"}
+    let user = req.query.user
+    let score = req.query.score
+    users[user] = score
+    res.status(200).send({"status":"success"})
+ })
+
+app.get("/highscores", (req ,res)=>{
+    headers={"http_status":200, "cache-control":  "no-cache"}
+    res.status(200).send(users)
+ })
+
+app.get('/auth', (req, res) => {
+    let user = rug.generate();
+    users[user] = 0
+    res.status(200).send({"user":user})
+ });
 
 app.listen(PORT , ()=>{
     console.log(`STARTED LISTENING ON PORT ${PORT}`)
